ec4c48ddeeeeddf2721ca3edb8e79aa1bf52b02b
Mauri <mauri@example.com>
2026-05-13 18:41:37 -0300

    final 3

--- relatorio_3/codigos/conv_m.m
+++ relatorio_3/codigos/conv_m.m
@@ -6,6 +6,16 @@ function [y, ny] = conv_m(x, nx, h, nh)
   % x = input sequence with support nx
   % h = impulse response with support nh
   
+  % Se nx for apenas o índice de início, gera o vetor completo
+  if length(nx) == 1
+    nx = nx:(nx + length(x) - 1);
+  end
+  
+  % Se nh for apenas o índice de início, gera o vetor completo
+  if length(nh) == 1
+    nh = nh:(nh + length(h) - 1);
+  end
+
   nyb = nx(1) + nh(1);
   nye = nx(end) + nh(end);
   ny = nyb:nye;
